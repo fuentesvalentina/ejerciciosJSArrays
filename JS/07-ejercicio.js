7f3b76e90468bcc1e4d39dae7447e7b653cec7e6
Valentina Fuentes <fuentesvalentina74@gmail.com>
2023-11-12 19:58:09 -0300

    falta un commit?

---
+++ JS/07-ejercicio.js
@@ -0,0 +1,29 @@
+/*Escriba un script que muestre la tabla de 
+multiplicar de un número ingresado por pantalla, 
+la creación de la tabla debe ser realizada con una función y
+ mostrar solo los resultados del 1 al 10 del número elegido por el usuario.
+ */
+
+ function mostrarTablaMultiplicar(numero) {
+    document.write("<h2>Tabla de Multiplicar del " + numero + "</h2>");
+    document.write("<table border='1'>");
+    document.write("<tr><th>Multiplicador</th><th>Resultado</th></tr>");
+
+    for (let i = 1; i <= 10; i++) {
+        const resultado = numero * i;
+        document.write("<tr><td>" + i + "</td><td>" + resultado + "</td></tr>");
+    }
+
+    document.write("</table>");
+}
+
+// Solicitar al usuario ingresar un número
+const numeroIngresado = parseFloat(prompt("Ingrese un número para mostrar su tabla de multiplicar:"));
+
+// Verificar que se ingresó un valor numérico
+if (isNaN(numeroIngresado)) {
+    document.write("Por favor, ingrese un número válido.");
+} else {
+    // Llamar a la función para mostrar la tabla de multiplicar
+    mostrarTablaMultiplicar(numeroIngresado);
+}
